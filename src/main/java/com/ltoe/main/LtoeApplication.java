4950d320ff994847e69d73d5e90be6aec706c40e
Rahul Rajendra Jadhav <Rahul.Jadhav@ltts.com>
2023-12-29 19:35:40 +0530

    disabling the command line runner method body

--- src/main/java/com/ltoe/main/LtoeApplication.java
+++ src/main/java/com/ltoe/main/LtoeApplication.java
@@ -34,14 +34,14 @@ public class LtoeApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		JobParameters jobParameters = new JobParametersBuilder()
-				.addDate("date", new Date())
-				.addString("uuid", UUID.randomUUID().toString())
-				.toJobParameters();
-		long startTime = System.currentTimeMillis();
-		this.jobLauncher.run(this.job, jobParameters);
-		long endTime = System.currentTimeMillis();
-		endTime = (endTime - startTime)/1000;
-		logger.info("total execution time(seconds): {}", endTime);
+//		JobParameters jobParameters = new JobParametersBuilder()
+//				.addDate("date", new Date())
+//				.addString("uuid", UUID.randomUUID().toString())
+//				.toJobParameters();
+//		long startTime = System.currentTimeMillis();
+//		this.jobLauncher.run(this.job, jobParameters);
+//		long endTime = System.currentTimeMillis();
+//		endTime = (endTime - startTime)/1000;
+//		logger.info("total execution time(seconds): {}", endTime);
 	}
 }
