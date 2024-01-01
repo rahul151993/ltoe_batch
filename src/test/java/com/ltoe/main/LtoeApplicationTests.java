bf53e0fce4ac0b896c4dec2d8d5f26a1e4aa5283
Rahul Rajendra Jadhav <Rahul.Jadhav@ltts.com>
2024-01-01 18:15:04 +0530

    initial commits for the changes of batches

--- src/test/java/com/ltoe/main/LtoeApplicationTests.java
+++ src/test/java/com/ltoe/main/LtoeApplicationTests.java
@@ -1,8 +1,15 @@
 package com.ltoe.main;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @ExtendWith(MockitoExtension.class)
 class LtoeApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(LtoeApplication.class);
+
 }
